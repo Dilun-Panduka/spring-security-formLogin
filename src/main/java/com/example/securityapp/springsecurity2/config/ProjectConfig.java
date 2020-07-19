66bc7a1aa55e744e7618bcdb8b7bd0c8b7fdba86
Dilun-Panduka <dilunp4@gmail.com>
2020-07-19 14:06:50 +0530

    initial commit

---
+++ src/main/java/com/example/securityapp/springsecurity2/config/ProjectConfig.java
@@ -0,0 +1,43 @@
+package com.example.securityapp.springsecurity2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("Dilun").password("12345").authorities("ADMIN").build();
+        UserDetails user2 = User.withUsername("Panduka").password("12345").authorities("USER").build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/hello", true);
+
+        http.authorizeRequests()
+                .mvcMatchers("/admin").access("hasAnyAuthority('ADMIN')")
+                .mvcMatchers("/main").permitAll()
+                .anyRequest().authenticated();
+    }
+}
